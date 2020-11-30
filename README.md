613acbc194453bed61ce1686e43aaab1398eb6a9
4cd87a <4cd87a@gmail.com>
2020-11-30 09:19:42 +0100

    Possible to choose a lecture if several is running
    Now it's possible to choose a lecture if several is running or nothing in live

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ This is a script that runs a zoom lecture depending on your schedule
 If you are as tired as I am of opening a excel with all links to your lectures before each zoom lecture. This script will save you a few clicks.
 
 # How this work?
-1) Python file `zoom_start.py` looks in `data.py` if you have lecture running
-2) `data.py` is example of data file with `lectures` as a array of your lectures in format
-`[weekday, start hour, end hour, link]` (weekday: 1 for Monday, 7 for Sunday)
-3) `Lecture now.bat` simple bat file (obviously for Windows) with command `python zoom_start.py`
+1) Python file `zoom_start.py` looks in `data.py` if you have lectures running
+2) `data.py` is example of a data file with `lectures` as a array of your lectures in format
+`[weekday, start hour, end hour, link, title for you (optional)]` (weekday: 1 for Monday, 7 for Sunday)
+3) `Lecture now.bat` is a simple bat file (obviously for Windows) with command `python zoom_start.py`
 
 # What you can do?
-* You can create a link of `Lecture now.bat` in `%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\[subfolder if you want]`, in this way you can tap `win`, type `[name of your link file]` and open script
-* You can set a shortcut for this script in Properties menu. And so open script by `ctrl+alt+l` (for example)
+* You can create a link of `Lecture now.bat` in `%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\[subfolder if you want]`, in this way you can tap `win` key, type `[name of your link file]` and open the script
+* You can set a shortcut for this script in the Properties menu. And so, for example, open script by `ctrl+alt+l`
--- zoom_start.py
+++ zoom_start.py
@@ -4,6 +4,7 @@ import os
 # import subprocess
 
 def openlink(s):
+	print("Openning {}".format(s))
 	os.system("start \"\" {}".format(s))
 	# os.chdir("{}\\Zoom\\bin".format(os.getenv('APPDATA')))
 	# print(s)
@@ -13,15 +14,42 @@ def look_and_run():
 	now = datetime.datetime.now()
 	week = now.isoweekday()
 	data.lectures.reverse()
+	lect = []
 	for d in data.lectures:
+		# print(d)
 		if d[0] == week:
 			t1 = datetime.datetime.combine(now.today(), datetime.datetime.strptime(d[1], '%H:%M').time())
 			t2 = datetime.datetime.combine(now.today(), datetime.datetime.strptime(d[2], '%H:%M').time())
 			if t1 - datetime.timedelta(minutes=15) < now and t2 > now:
-				print(d[3])
-				if 'zoom' in d[3]: openlink(d[3])
-				if 'gotomeeting' in d[3]: openlink(d[3])
-				return True
+				lect.append(d)
+
+	if len(lect)==1:
+		if 'zoom' in lect[0][3]: openlink(lect[0][3])
+		if 'gotomeeting' in lect[0][3]: openlink(lect[0][3])
+		return True
+
+	elif len(lect):
+		lect.reverse()
+		print("Choose lecture :")
+		for i, d in enumerate(lect):
+			print("{} -> {} ({}-{} {})".format(i+1,d[3] if len(d)==4 else d[4],d[1],d[2],['all','Mon','Tue','Wed','Thu','Fri','Sat','Sun'][d[0]]))
+
+		l = int(input("choice (put 0 for nothing) : "))
+		if l>0:
+			if 'zoom' in lect[l-1][3]: openlink(lect[l-1][3])
+			if 'gotomeeting' in lect[l-1][3]: openlink(lect[l-1][3])
+			return True
+
+	print("Nothing is now. Choose your lecture from all:")
+	data.lectures.reverse()
+	for i, d in enumerate(data.lectures):
+		print("{} -> {} ({}-{} {})".format(i+1,d[3] if len(d)==4 else d[4],d[1],d[2],['all','Mon','Tue','Wed','Thu','Fri','Sat','Sun'][d[0]]))
+
+	l = int(input("choice (put 0 for nothing) : "))
+	if l>0:
+		if 'zoom' in data.lectures[l-1][3]: openlink(data.lectures[l-1][3])
+		if 'gotomeeting' in data.lectures[l-1][3]: openlink(data.lectures[l-1][3])
+		return True
 
 	return False
 
